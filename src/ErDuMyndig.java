d1f028d37b41342bbfdb0a19f549d958b038301c
Fadl Matar <fadlmatar00@gmail.com>
2019-09-27 08:42:23 +0200

    initail commit.

---
+++ src/ErDuMyndig.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ErDuMyndig {
+    public static void main(String[] args) {
+        Scanner keyb = new Scanner(System.in);
+        int age;
+        age = keyb.nextInt();
+        if (age<=18)
+        System.out.println("Du er ikke gammel nok");
+        else
+        System.out.println("Tillykke fødselsdagen Moussa");
+
+    }
+}
