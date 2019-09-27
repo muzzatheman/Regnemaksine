01ba103e2fab98cf1ebd608ae18c049553d99fd4
Fadl Matar <fadlmatar00@gmail.com>
2019-09-27 08:47:45 +0200

    initail commit.

---
+++ src/Regnemaksine.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.lang.String;
+
+public class Regnemaksine {
+    public static void main(String[] args) {
+        System.out.println("Indtast venligst dine værdier her:");
+        Scanner input = new Scanner(System.in);
+        float a = input.nextFloat();
+        float f = input.nextFloat();
+        String username = input.nextLine();
+        input.close();
+
+        System.out.println("Addition: " + (a+f));
+        System.out.println("Subtraktion: " + (a-f));
+        System.out.println("Multiplikation: " + (a*f));
+        System.out.println("Division: " + (a/f));
+        System.out.println("Modulus: " + (a%f));
+        System.out.println("sqrt x: " + Math.pow(a,f));
+        System.out.println("Procent: " + (a/f)*100);
+
+    }
+}
